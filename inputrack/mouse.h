be9d877a1340d9e25f93e981d874cb6942edb558
0dminnimda <0dminnimda@gmail.com>
2022-08-16 23:03:05 +0300

    🏷️inputrack\mouse.h: include assert

--- inputrack/mouse.h
+++ inputrack/mouse.h
@@ -5,6 +5,12 @@ typedef int bool;
 static const bool false = 0;
 static const bool true = 1;
 
+#ifndef __cplusplus
+#   include <assert.h>
+#else
+#   include <cassert>
+#endif
+
 typedef struct Position {
     long x;
     long y;
